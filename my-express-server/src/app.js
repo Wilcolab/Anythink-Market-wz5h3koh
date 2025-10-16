3ed80f0e68fa007eef344f9cc0ae1c5b00048703
Arjan kc <arjankc@gmail.com>
2025-10-16 05:57:02 +0000

    Add package.json with express dependency

--- my-express-server/src/app.js
+++ my-express-server/src/app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8001;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+let tasks = [
+    "Write a diary entry from the future",
+    "Create a time machine from a cardboard box",
+    "Plan a trip to the dinosaurs",
+    "Draw a futuristic city",
+    "List items to bring on a time-travel adventure"
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/tasks', (req, res) => {
+    const task = req.body;
+    tasks.push(task.text);
+    res.json({ message: 'Task added successfully' });
+});
+
+app.get('/tasks', (req, res) => {
+    res.json({ tasks: tasks });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
